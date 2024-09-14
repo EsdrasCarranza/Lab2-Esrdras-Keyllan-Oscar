f0168ea2c11640f2a5c014612ad7b0d39994803c
50488 <50488@192.168.0.19>
2024-09-14 08:47:12 -0600

    clase song terminada

---
+++ Carranza_Esdras_LAB#8/src/carranza_esdras_lab/pkg8/Carranza_Esdras_LAB8.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package carranza_esdras_lab.pkg8;
+
+/**
+ *
+ * @author 50488
+ */
+public class Carranza_Esdras_LAB8 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
